818828cc668a7b06a2f6c96048ac044dc4121db5
Sree <sree@gmail.com>
2020-03-17 15:17:49 +0530

    initial commit

---
+++ src/main/java/hello/HelloDemoApplication.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDemoApplication.class, args);
+	}
+
+}
+@RestController
+class HelloRestController{
+	@RequestMapping("/index/{name}")
+	public String getName(@PathVariable(value = "name") String name){
+		String response = "Hai " + name + ", message received on: " + new Date();
+		return response;
+	}
+
+
+}
