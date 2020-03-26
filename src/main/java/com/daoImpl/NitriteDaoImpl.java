5e9fdafbc519dd0d258f1791d2063fca55ba3155
Sree <sree@gmail.com>
2020-03-26 17:28:56 +0530

    File based nitrite added

--- src/main/java/com/daoImpl/NitriteDaoImpl.java
+++ src/main/java/com/daoImpl/NitriteDaoImpl.java
@@ -3,7 +3,6 @@ import com.dao.NitriteDao;
 import com.model.Employee;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.Cursor;
-import org.dizitart.no2.objects.ObjectFilter;
 import org.dizitart.no2.objects.ObjectRepository;
 import org.dizitart.no2.objects.filters.ObjectFilters;
 import org.springframework.stereotype.Repository;
@@ -11,6 +10,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public class NitriteDaoImpl implements NitriteDao {
+	// For file based DB -> un comment the below line and comment line no:16
+	// Nitrite db = Nitrite.builder().filePath("tmp/test.db").autoCommitBufferSize(2048).openOrCreate();
+	//For In memory DB
 	Nitrite db = Nitrite.builder().openOrCreate();
 	ObjectRepository<Employee> employeeStore = db.getRepository(Employee.class);
 	@Override
@@ -20,11 +22,13 @@ public class NitriteDaoImpl implements NitriteDao {
 	@Override
 	public void addEmployee(Employee employee) {
 		employeeStore.insert(employee);
+
 	}
 	@Override
 	public void addEmployees(List<Employee> employees) {
 		Employee[] emp = employees.toArray(new Employee[0]);
 		employeeStore.insert(emp);
+
 	}
 	@Override
 	public void updateEmployee(Employee employee) {
